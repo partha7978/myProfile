245c753c251ba8afd0405bb7ea065a83a0484d0f
partha7978 <muduliparthasarathi@gmail.com>
2021-07-01 18:41:22 +0530

    adding dark mode

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
 const btn = document.querySelector(".btn-toggle");
-// const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-
-// const currentTheme = localStorage.getItem("theme");
-// if (currentTheme == "dark") {
-//   document.body.classList.toggle("dark-theme");
-// } else if (currentTheme == "light") {
-//   document.body.classList.toggle("light-theme");
-// }
-
-// btn.addEventListener("click", function () {
-//   if (prefersDarkScheme.matches) {
-//     document.body.classList.toggle("light-theme");
-//     var theme = document.body.classList.contains("light-theme")
-//       ? "light"
-//       : "dark";
-//   } else {
-//     document.body.classList.toggle("dark-theme");
-//     var theme = document.body.classList.contains("dark-theme")
-//       ? "dark"
-//       : "light";
-//   }
-//   localStorage.setItem("theme", theme);
-// });
 
 const icon = document.querySelector(".icon");
 
@@ -33,6 +10,7 @@ btn.addEventListener("click", function () {
     icon.classList.toggle("fa-sun");
     icon.classList.toggle("far");
     icon.classList.toggle("fa-moon");
+    btn.classList.toggle("btn-color-change");
     btn.classList.toggle("dark-button-shadow");
 
 })
