37f06add7875b4c9bd7a1a44aee55770b4a1ba91
partha7978 <muduliparthasarathi@gmail.com>
2021-07-02 15:59:33 +0530

    optimising Dark Mode

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 const btn = document.querySelector(".btn-toggle");
 
 const icon = document.querySelector(".icon");
-<<<<<<< HEAD
 
 const secondPageLeftContent = document.querySelector(".right-side-background");
 const secondPageRightContent = document.querySelector(".left-side-background");
 
-
-
-=======
-const secondPageLeftContent = document.querySelector(".right-side-background");
-const secondPageRightContent = document.querySelector(".left-side-background");
->>>>>>> DarkMode
 btn.addEventListener("click", function () {
     var element = document.body;
     element.classList.toggle("dark-mode");
@@ -22,16 +15,8 @@ btn.addEventListener("click", function () {
     icon.classList.toggle("fa-moon");
     btn.classList.toggle("btn-color-change");
     btn.classList.toggle("dark-button-shadow");
-<<<<<<< HEAD
-
     
     secondPageLeftContent.classList.toggle("dark-mode");
     secondPageRightContent.classList.toggle("dark-mode");
 
-
-=======
-    
-    secondPageLeftContent.classList.toggle("dark-mode");
-    secondPageRightContent.classList.toggle("dark-mode");
->>>>>>> DarkMode
 })
