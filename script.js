e0573a886981b53cd78ba20976f5d7b740f014dc
partha7978 <muduliparthasarathi@gmail.com>
2021-07-02 15:37:40 +0530

    adding dark mode

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const btn = document.querySelector(".btn-toggle");
 
 const icon = document.querySelector(".icon");
-
+const secondPageLeftContent = document.querySelector(".right-side-background");
+const secondPageRightContent = document.querySelector(".left-side-background");
 btn.addEventListener("click", function () {
     var element = document.body;
     element.classList.toggle("dark-mode");
@@ -12,5 +13,7 @@ btn.addEventListener("click", function () {
     icon.classList.toggle("fa-moon");
     btn.classList.toggle("btn-color-change");
     btn.classList.toggle("dark-button-shadow");
-
+    
+    secondPageLeftContent.classList.toggle("dark-mode");
+    secondPageRightContent.classList.toggle("dark-mode");
 })
