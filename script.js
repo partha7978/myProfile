dded4f4c2fcfd355d17e3e86ca18124179fa8e28
partha7978 <muduliparthasarathi@gmail.com>
2021-07-05 16:25:52 +0530

    optimizing js

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 const btn = document.querySelector(".btn-toggle");
-
 const icon = document.querySelector(".icon");
-
 const secondPageLeftContent = document.querySelector(".right-side-background");
 const secondPageRightContent = document.querySelector(".left-side-background");
-
-
 const firstPageProfileText = document.querySelector(".profile-text");
-
 const thirdPageContainer = document.querySelector(".third-container");
 const thirdPageHeading = document.querySelector(".project-color");
 
-
-
-
 btn.addEventListener("click", function () {
-    var element = document.body;
+   darkMode();
+    firstPage();
+    secondPage();
+    thirdPage();
+})
+
+const darkMode = () => {
+    let element = document.body;
     element.classList.toggle("dark-mode");
-   
+}
+
+const firstPage = () => {
     icon.classList.toggle("fas");
     icon.classList.toggle("fa-sun");
     icon.classList.toggle("far");
     icon.classList.toggle("fa-moon");
     btn.classList.toggle("btn-color-change");
     btn.classList.toggle("dark-button-shadow");
-    
-  
-    secondPage();
-    
+    //These are for the dark mode button 
     firstPageProfileText.classList.toggle("dark-mode");
-    thirdPageContainer.classList.toggle("third-page-dark-mode");
-    thirdPageHeading.classList.toggle("project-color-dark-mode");
-})
-
+}
 
 const secondPage = () => {
     secondPageLeftContent.classList.toggle("dark-mode");
     secondPageRightContent.classList.toggle("dark-mode");
+}
+
+const thirdPage = () => {
+    thirdPageContainer.classList.toggle("third-page-dark-mode");
+    thirdPageHeading.classList.toggle("project-color-dark-mode");
 }
